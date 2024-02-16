d930b41bdcc007ef93514a79d0dbbd6804320f5d
Piotr Pawlak <piotr.mikolaj.pawlak@gmail.com>
2024-02-16 12:26:32 +0100

    api: get and delete games

--- api/routes/games.js
+++ api/routes/games.js
@@ -24,4 +24,56 @@ router.post("", (req, res, next) => {
   }
 );
 
+router.delete("/:id", (req, res, next) => {
+    Game.deleteOne({ _id: req.params.id })
+      .then(result => {
+        console.log(result);
+        if (result.deletedCount > 0) {
+          res.status(200).json({ message: `Game with id ${req.params.id} deleted`});
+        } else {
+          res.status(404).json({ message: `Game with id ${req.params.id} not found` });
+        }
+      })
+      .catch(error => {
+        res.status(400).json({
+          message: `Error deleting game with id ${req.params.id}`,
+          error: error
+        });
+      });
+});
+
+router.get("", (req, res, next) => {
+    const gamesQuery = Game.find();
+
+    gamesQuery.then(games => {
+        res.status(200).json({
+            message: "Games fetched successfully",
+            games: games,
+        });
+      })
+      .catch(error => {
+        res.status(400).json({
+          message: "Fetching games failed",
+          error: error
+        });
+      });
+});
+
+router.get("/:id", (req, res, next) => {
+    Game.findById(req.params.id)
+      .then(game => {
+        if (game) {
+          res.status(200).json(game);
+        } else {
+          res.status(404).json({ message: `Game with id ${req.params.id} not found` });
+        }
+      })
+      .catch(error => {
+        res.status(400).json({
+          message: "Fetching game failed",
+          error: error
+        });
+      });
+});   
+
 module.exports = router;
